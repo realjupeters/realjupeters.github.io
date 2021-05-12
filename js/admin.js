03ff5869ffa412f0fc9feb05bdcee7b8e8a25187
LoggeL <hyper.xjo@gmx.de>
2021-05-13 01:55:45 +0200

    admin page framework

--- js/admin.js
+++ js/admin.js
@@ -13,13 +13,48 @@ if (!token) {
     alert('No token present. Please login first.')
 }
 
-fetch(BASE_ENDPOINT_URL + 'admin/test', {
+// registration
+fetch(BASE_ENDPOINT_URL + 'admin/poolparty/account', {
     method: 'get',
     headers: new Headers({
         'Authorization': token,
     })
-}).then(response => {
-    response.json().then(json => {
-        console.log(json)
-        var data = json.data
-        if (data.anmeldung.length > 0) {
+}).then(async response => {
+    const json = await response.json()
+    console.log(json)
+})
+
+
+// registration
+fetch(BASE_ENDPOINT_URL + 'admin/poolparty/registration', {
+    method: 'get',
+    headers: new Headers({
+        'Authorization': token,
+    })
+}).then(async response => {
+    const json = await response.json()
+    console.log(json)
+})
+
+// item
+fetch(BASE_ENDPOINT_URL + 'admin/poolparty/item', {
+    method: 'get',
+    headers: new Headers({
+        'Authorization': token,
+    })
+}).then(async response => {
+    const json = await response.json()
+    console.log(json)
+})
+
+// volunteer
+fetch(BASE_ENDPOINT_URL + 'admin/poolparty/volunteer', {
+    method: 'get',
+    headers: new Headers({
+        'Authorization': token,
+    })
+}).then(async response => {
+    const json = await response.json()
+    console.log(json)
+})
+
